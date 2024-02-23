d12a1a8bb1a60b165cdc1243bb2a252b778269e4
unechevre <enzonezo1@gmail.com>
2024-02-23 15:40:18 +0100

    mise en place de la base effective de la racine du projet dans un but de commencer sur de bonnes bases

---
+++ src/main/java/com/kouwik/KouwikApplication.java
@@ -0,0 +1,13 @@
+package com.kouwik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KouwikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KouwikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kouwik/KouwikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kouwik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KouwikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
